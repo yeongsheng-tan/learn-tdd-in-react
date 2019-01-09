ddb7641d2891b09aaf3340cdc28cb8edcdd53d3e
Tan Yeong Sheng <tanyeongsheng@gmail.com>
2019-01-10 00:34:04 +0800

    component test for button click handler to clear message

--- src/NewMessageForm.js
+++ src/NewMessageForm.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
 
 export default class NewMessageForm extends Component {
+  state = { inputText: '' }
+
+  handleTextChange = (event) => {
+    this.setState({ inputText: event.target.value });
+  }
+
+  handleSend = () => {
+    const { inputText } = this.state;
+    const { onSend } = this.props;
+
+    onSend(inputText);
+
+    this.setState({ inputText: '' });
+  }
+
   render() {
+    const { inputText } = this.state;
     return (
       <div>
         <input
           type="text"
           data-test="messageText"
+          value={ inputText }
+          onChange={ this.handleTextChange }
         />
         <button
           data-test="sendButton"
-            >
-            Send
+          onClick={ this.handleSend }
+        >
+          Send
         </button>
       </div>
     );
